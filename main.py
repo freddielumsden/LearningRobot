c9902a61afb15f75fd1835d461be1b9f2464ca3b
freddielumsden <freddielumsden@gmail.com>
2022-02-19 21:03:54 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class Robot(self):
+	def getMessage():
+		self.message = input("")
+		return message
+	def processMessage():
+		pass
+	def sendMessage(message):
+		pass
+	def addMessage(message):
+		pass
+
+
+print("Welcome to the greetings bot!!!!\nPress enter to quit.")
+robot = Robot()
+message = robot.getMessage()
+while message != "":
+	message_understood = robot.processMessage()
+	message = robot.getMessage()
